73abcb3a0a71005c788d5c4382c60f0f85dbfdf4
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-10 08:35:04 -0600

    set up routing and pages to be routed to

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef, useCallback } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Search the <a
+            className="App-link"
+            href="https://spoonacular.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          > Spoonacular API</a> for awesome recipes and information
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+      </nav>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/routes/search.jsx
@@ -0,0 +1,7 @@
+export default function Search() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Food Magic Search</h2>
+    </main>
+  )
+}
